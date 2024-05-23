1c6b6557a5628777a79cdf7a85326e7eb5af062a
LisaUA <alontshka.777@gmail.com>
2024-05-23 16:53:58 +0300

    #2hwjs 9

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+// import * from './js/1-gallery.js';
+
+// import { taskTitle } from './js/2-form.js';
+
+console.log(taskTitle);
